494d5760b93d148fcf1c9457e133f5746d5beb05
anudeeps28 <anudeepsharma28@gmail.com>
2022-09-12 11:35:09 -0400

    variablesa

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
+#include <iostream> // putting iostream file into the file before compiling
 using namespace std;
 
+// this whole thing below is a subroutine: a block of code
 int main(){
-    cout << "Hello World!" << endl;
+
+    cout << "Starting program..." << flush;
+
+    cout << "banana." << "apple." << "Orange." << endl;
+
+    // cout is used for outputting text in c++
+    cout << "Hello Wo to h rld!" << endl; 
+    cout << "this is some more text" << endl;
     return 0;
 }
 
