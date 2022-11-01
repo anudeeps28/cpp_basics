692d3d3a7c1363705d6093ebd15330fdd3396e59
anudeeps28 <anudeepsharma28@gmail.com>
2022-10-31 23:31:56 -0400

    ordered set

--- STL::C++/Sets/setsBasics.cpp
+++ STL::C++/Sets/setsBasics.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 void print(set<string> &s) {
-    
+    for (auto value: s) {
+        cout << value << endl;
+    }
 }
 
 int main() {
@@ -13,6 +15,22 @@ int main() {
     s.insert("zsfds");
     s.insert("bycdf");
 
+    print(s);
 
-    
+    auto it = s.find("abc");
+    s.erase(it);
+    print(s);
+
+    // .erase can take iterator as well as the value 
+    s.erase("bycdf");
+    print(s);
+
+    auto itt = s.find("fsdg");
+    if (itt != s.end()) { // this if condition is present to prevent segmentation fault so that if iterator does not find any value and jumps to the end, it exits out of loop and does and perform the earse operation
+        s.erase(itt);       
+
+    }
+    print(s);
+    // all the same method that apply on maps, will apply on sets also
+        // like - erase, etc.
 }
