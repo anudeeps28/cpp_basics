eaed071d6d291788e29173ae5bea2e65702b463e
anudeeps28 <anudeepsharma28@gmail.com>
2022-10-31 23:13:27 -0400

    Sets basics C++

---
+++ STL::C++/Sets/setsBasics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+void print(set<string> &s) {
+    
+}
+
+int main() {
+    set<string> s; // we can make a set of anything, just like a map/vector
+    s.insert("abc");
+    s.insert("zsfds");
+    s.insert("bycdf");
+
+
+    
+}
