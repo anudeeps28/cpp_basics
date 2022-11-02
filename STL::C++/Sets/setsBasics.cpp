5d35a611046cbaff322c8e305eae7c50f972fa2e
anudeeps28 <anudeepsharma28@gmail.com>
2022-11-01 22:24:14 -0400

    multisets

--- STL::C++/Sets/setsBasics.cpp
+++ STL::C++/Sets/setsBasics.cpp
@@ -10,26 +10,42 @@ void print(set<string> &s) {
     }
 }
 
-void print(unordered_set<int> &s2) {
-    for (auto value: s2) {
+void print2(unordered_set<int> &s) {
+    for (auto value: s) {
         cout << value << endl;
     }
 }
+
+void print3(multiset<int> &s) {
+    for (auto value: s) {
+        cout << value << endl;
+    }
+}
+
 int main() {
     set<string> s; // we can make a set of anything, just like a map/vector
-    s.insert("abc");
+    s.insert("abc"); // O(lon(n))
     s.insert("zsfds");
     s.insert("bycdf");
 
     unordered_set<int> s2;
-    s2.insert(1); 
+    s2.insert(1); // O(1)
     s2.insert(4);
     s2.insert(2);
     s2.insert(7);
     s2.insert(5);
 
+    multiset<int> s3;
+    s3.insert(1); // O(lon(n))
+    s3.insert(4);
+    s3.insert(1);
+    s3.insert(7);
+    s3.insert(5);
+    s3.insert(4);
+
     print(s);
-    print(s2);
+    print2(s2);
+    print3(s3);
 
     auto it = s.find("abc"); 
     s.erase(it);
@@ -47,4 +63,12 @@ int main() {
     print(s);
     // all the same method that apply on maps, will apply on sets also
         // like - erase, etc.
+
+    auto ittt = s3.find(1);
+    s3.erase(ittt);
+    print3(s3);
+
+    // s3.erase(1) -> this will erase all 1 in the set
+    // the same functions apply to set as do in maps: .size(), .clear, 
+    // these are all the common functions that we can use for all containers
 }
