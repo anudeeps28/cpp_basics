6e34cbe2fa595eb5a96536178a9a1b4903d918df
anudeeps28 <anudeepsharma28@gmail.com>
2022-11-08 21:24:50 -0500

    complex nesting

--- STL::C++/Nesting in STL containers/nestingConcepts.cpp
+++ STL::C++/Nesting in STL containers/nestingConcepts.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main () {
+int main() {
 
-    return 0
+    // this is an example of a nested map
+    map<pair<string, string>, vector<int>> m;
+    m[{"S", "D"}] = {2, 3, 4, 5};
+    m[{"A", "S"}] = {1,2,3,4};
+
+    for (auto i: m) {
+        auto fullName = i.first;
+        auto list = i.second;
+
+        cout << fullName.first << " " << fullName.second << endl;
+
+        for (auto i: list) {
+            cout << i << endl;
+        }
+    }
+    
+    // NOTE - all the keys of the map are stored in sorted order, even if the keys are pairs and vectors themselves
+    // even though we added SD first, AD is printed first because it is stored first in the map (sorted order)
 }
