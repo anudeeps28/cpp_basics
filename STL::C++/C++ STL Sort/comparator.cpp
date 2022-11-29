3dc731b7e4068a21d0b3e43f503e2cff5de521b5
anudeeps28 <anudeepsharma28@gmail.com>
2022-11-28 21:34:10 -0500

    custom sort

---
+++ STL::C++/C++ STL Sort/comparator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+bool cmp(int a, int b) {
+    return a > b; // return the condition that you want to implement
+}
+
+int main() {
+    int n2;
+    cin >> n2;
+    vector<int> V(n2);
+    
+    for (int i = 0; i < n2; i++) {
+        cin >> V[i];
+    }
+
+    sort(V.begin(), V.end(), cmp); 
+
+    for (auto i: V) {
+        cout << i << endl;
+    }
+}
