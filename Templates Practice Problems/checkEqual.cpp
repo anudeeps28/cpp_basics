16427da06343b89e8934d9a109928a308214b48a
anudeeps28 <anudeepsharma28@gmail.com>
2023-02-13 21:51:03 -0500

    final solution

--- Templates Practice Problems/checkEqual.cpp
+++ Templates Practice Problems/checkEqual.cpp
@@ -1,20 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-template<typename T>
-bool isEqual(T x, T y) {
-    return x == y;
+template <typename T>
+bool are_equal(T a, T b) {
+    return a == b;
 }
 
 int main() {
-    int a = 2;
-    int b = 3;
-    bool intEqual = isEqual(a,b);
-    cout << intEqual << endl;
-
-    float c = 2.3;
-    float d = 4.6;
-    bool floatEqual = isEqual(c,d);
-    cout << floatEqual << endl;
-
+    int x = 5;
+    int y = 7;
+    double a = 3.14;
+    double b = 3.14;
+    
+    bool intEqual = are_equal(x, y);
+    bool doubleEqual = are_equal(a, b);
+    
+    if (intEqual) {
+        cout << x << " and " << y << " are equal." << endl;
+    } else {
+        cout << x << " and " << y << " are not equal." << endl;
+    }
+    
+    if (doubleEqual) {
+        cout << a << " and " << b << " are equal." << endl;
+    } else {
+        cout << a << " and " << b << " are not equal." << endl;
+    }
+    
+    return 0;
 }
